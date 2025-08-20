1f2db6deebfe20f4cd240e1614c4931a9559af23
Luis Alberto <luisro43@gmail.com>
2025-08-20 02:14:25 +0000

    modifi

--- pages/Page1/jsobjects/voiceRecognitionUtils/voiceRecognitionUtils.js
+++
@@ -1,9 +0,0 @@
-export default {
-  startRecognition: async () => {
-    // Definimos la función que le enviará un mensaje al navegador
-    await postWindowMessage(
-      'start_speech_recognition', 
-      'window' // 'window' significa que enviamos el mensaje al navegador
-    );
-  }
-}
